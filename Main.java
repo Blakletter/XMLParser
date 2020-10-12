00fc52c0cb32f048ba0360d049baeb649b1fa4e3
Blakletter <48567475+Blakletter@users.noreply.github.com>
2020-10-12 12:16:53 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,14 +16,11 @@ public class Main {
             Node node = parser.ParseXML();
             System.out.println("Parsing completed in " + (System.currentTimeMillis()-milli)/1000.0 + " Seconds.");
 
-            ArrayList<Node> m = node.findNodes("author");
+            ArrayList<Node> m = node.findNodes("main");
             System.out.println(m.size());
             for (Node value : m) {
-                //String attr = value.getAttribute("id");
                 System.out.println(value.getData());
             }
-
-
         });
         thread.start();
     }
